1ae9afca794e8414899ad05bd19e1fa5d6764ea2
ayezinzu <59067019+ayezinzu@users.noreply.github.com>
2023-04-03 06:36:47 +0530

    adding individual fetching

--- netlify/functions/fetchVacancies/fetchVacancies.ts
+++ netlify/functions/fetchVacancies/fetchVacancies.ts
@@ -2,17 +2,33 @@ import { Handler } from "@netlify/functions";
 import { getVacancies } from "../../../src/api/methods";
 
 export const handler: Handler = async (event, context) => {
+  let vacancyData;
+
   const result = await getVacancies();
+  vacancyData = result.data.data;
+  if (event.queryStringParameters?.id) {
+    const passedId = event.queryStringParameters.id.toString();
+
+    const allVacancies = vacancyData;
+    const foundVacancy = allVacancies.find(
+      (eachVacancy) => eachVacancy.id.toString() === passedId.toString()
+    );
+    if (foundVacancy) {
+      vacancyData = foundVacancy;
+    } else {
+      vacancyData = null;
+    }
+  }
   const headers = {
     "Content-Type": "application/json; charset=utf-8",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
   };
-  if (result.success) {
+  if (vacancyData || result.success) {
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify(result.data.data),
+      body: JSON.stringify(vacancyData),
     };
   } else {
     return {
